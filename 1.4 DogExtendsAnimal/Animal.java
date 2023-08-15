672798c91f9f07fa5ec3c385697048df91ec8a0e
[your <tw841117@gmail.com>
2023-08-15 11:32:40 -0400

    update 1.4 Animal

--- 1.4 DogExtendsAnimal/Animal.java
+++ 1.4 DogExtendsAnimal/Animal.java
@@ -6,8 +6,20 @@
  */
 public class Animal {
 
+	String name = "Bob";
+	
 	public void greeting() {
 		System.out.println("I am an animal.");
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		// cast o to Animal
+		Animal otherAnimal = (Animal) o;
+		
+		// compare this.name to name of the animal passed in
+		return this.name.equals(otherAnimal.name);
 	}
 	
 	public static void main(String[] args) {
@@ -16,7 +28,8 @@ public class Animal {
 		
 		// override toString() in a class and to return something "meaningful"
 		// originally: Dog@4517d9a3
-		System.out.println(dog);
+		System.out.println(dog);	
+	
+		System.out.println(dog.equals(dog));
 	}
-
 }
